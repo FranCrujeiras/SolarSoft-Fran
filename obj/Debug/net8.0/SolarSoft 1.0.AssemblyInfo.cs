3c45c9120004fb4cdec96fba25161f792009fb72
FranCrujeiras <rodriguez.cr.fran1997@gmail.com>
2024-04-04 17:30:11 +0200

    04/04 GET y POST en Front
    Casi completado el formulario del POST

--- obj/Debug/net8.0/SolarSoft 1.0.AssemblyInfo.cs
+++ obj/Debug/net8.0/SolarSoft 1.0.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolarSoft 1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54c5f9206d99a08b9eaec23aada3fdaaf1ea3021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e55e3a13061e13ca7d9161fed24fb9ce9e3ae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolarSoft 1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolarSoft 1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
