cd2afe2f3035c9e12f293f3f2ac0e1089cfca2a9
FranCrujeiras <rodriguez.cr.fran1997@gmail.com>
2024-04-09 12:53:21 +0200

    09/04 Formulario finalizado con resultados

--- obj/Debug/net8.0/SolarSoft 1.0.AssemblyInfo.cs
+++ obj/Debug/net8.0/SolarSoft 1.0.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolarSoft 1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4cd356f3c4abd70bda5e9c43bd7f7e124186682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f6ed403ad3508559e7c8dc6af3098955398309")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolarSoft 1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolarSoft 1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
