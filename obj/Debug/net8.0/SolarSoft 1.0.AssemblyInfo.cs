142cec07aedd987614cf230084ac3fe0ea146182
FranCrujeiras <rodriguez.cr.fran1997@gmail.com>
2024-04-18 10:20:06 +0200

    17/04 Formulario de paneles acabado

--- obj/Debug/net8.0/SolarSoft 1.0.AssemblyInfo.cs
+++ obj/Debug/net8.0/SolarSoft 1.0.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolarSoft 1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af1f84bdf716558078c5604afb9927286a87d70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80446895dc1f0268b978c00765f97d63cd956dbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolarSoft 1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolarSoft 1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
